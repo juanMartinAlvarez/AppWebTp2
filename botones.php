3549927c7cc2f0f936a65500fcd163263d6b7fa8
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-06-25 19:47:53 -0300

    order botones code

--- botones.php
+++ botones.php
@@ -1,43 +1,59 @@
+
+<?php
+require_once('crud.php');
+
+$listaTareas = obtenerTareas();
+?>
+
+<?php
+if(isset($_GET['obtenerTareas'])) foreach($listaTareas as $tarea) {?>      
+<tr>
+	<td><?php echo $tarea->getid()?></td>
+	<td><?php echo $tarea->getfecha() ?> </td>
+	<td><?php echo $tarea->getdescripcion() ?> </td>
+	<td><?php echo $tarea->gettiempoasignado() ?> </td>
+	<td><?php echo $tarea->getintegranteasignado() ?> </td>
+	<td><?php echo $tarea->getidintegranteasignado() ?> </td>
+	<td><?php echo $tarea->getobservaciones() ?> </td>
+</tr>
+<?php 
+} ?>
+
+<?php
+  if (isset($_POST['idintegrante'])) {
+	$idintegrante = $_POST['idintegrante'];
+	$idtareamodif = $_POST['idtareamodif'];
+
+	echo modificarTareas($idintegrante, $idtareamodif);
+  }
+?>
+<?php
+  if (isset($_POST['idtareaborrar'])) {
+	$idtareaborrar = $_POST['idtareaborrar'];
+
+	echo borrarTarea($idtareaborrar);
+
+  }
+?>
+
 <!DOCTYPE html> 
 <html> 
 <head>
-
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
+	
+
 <body>
   
   <!-- mostrar las tareas existentes -->
   
-
-  
   <h2>
     Lista de tareas actualmente en la base de datos
   </h2>
-
-    <?php
-    require_once('crud.php');
-
-    $listaTareas = obtenerTareas();
-    ?>
-  
-
     <a href="llenarcamposintegrantes.php">crear tarea </a><br> <br>
 
     <a href="?obtenerTareas">obtener tareas </a> <br> <br>
-    <?php
-  
 
-    if(isset($_GET['obtenerTareas'])) foreach($listaTareas as $tarea) {?>      
-    <tr>
-        <td><?php echo $tarea->getid()?></td>
-        <td><?php echo $tarea->getfecha() ?> </td>
-        <td><?php echo $tarea->getdescripcion() ?> </td>
-        <td><?php echo $tarea->gettiempoasignado() ?> </td>
-        <td><?php echo $tarea->getintegranteasignado() ?> </td>
-        <td><?php echo $tarea->getidintegranteasignado() ?> </td>
-        <td><?php echo $tarea->getobservaciones() ?> </td>
-    </tr>
-
-    <?php } ?>
     
     <h2>modificar tareas</h2>
 
@@ -47,14 +63,6 @@
       <input type="submit" name="submit1" value="Modificar tarea">
 
       <div id="modiftareas">
-        <?php
-          if (isset($_POST['idintegrante'])) {
-            $idintegrante = $_POST['idintegrante'];
-            $idtareamodif = $_POST['idtareamodif'];
-
-            echo modificarTareas($idintegrante, $idtareamodif);
-          }
-        ?>
 
       </div>
     </form>
@@ -67,14 +75,7 @@
       <input type="submit" name="submit2" value="Borrar tarea">
 
       <div id="borrartareas">
-      <?php
-          if (isset($_POST['idtareaborrar'])) {
-            $idtareaborrar = $_POST['idtareaborrar'];
 
-            echo borrarTarea($idtareaborrar);
-
-          }
-        ?>
       </div>
 
     <br> <br>
