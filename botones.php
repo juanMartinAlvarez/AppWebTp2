4bf39a15cd3cbb04e8a4a90d942ebabdd4f9eebb
LucasNCaruso <caruso.lucas.nahuel@gmail.com>
2020-06-25 21:57:15 -0300

    Se agrega columnas al hacer el select

--- botones.php
+++ botones.php
@@ -6,8 +6,10 @@ $listaTareas = obtenerTareas();
 ?>
 
 <?php
-if(isset($_GET['obtenerTareas'])) foreach($listaTareas as $tarea) {?>      
-<tr><br>
+if(isset($_GET['obtenerTareas']))
+echo ' ID || FECHA || DESCRIPCION || TIEMPO ASIGNADO || ID INTEGRANTE || INTEGRANTE ASIGNADO || OBSERVACIONES'; 
+ foreach($listaTareas as $tarea) {?> 
+  <tr><br>
 	<td><?php echo $tarea->getid().' ||'?></td>
 	<td><?php echo $tarea->getfecha().' ||' ?> </td>
 	<td><?php echo $tarea->getdescripcion().' ||' ?> </td>
